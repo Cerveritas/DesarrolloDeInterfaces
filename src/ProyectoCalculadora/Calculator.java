df896b953ce878b8da3c93d4aa8404196fc7ce1f
SergioCerveraJ <seceji@gmail.com>
2023-10-16 14:02:33 +0200

    16/10/23

--- src/ProyectoCalculadora/Calculator.java
+++ src/ProyectoCalculadora/Calculator.java
@@ -2,6 +2,7 @@ package ProyectoCalculadora;
 
 import javax.swing.*;
 import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -21,39 +22,75 @@ public class Calculator {
 
 
         JFrame frame = new JFrame("CALCULATOR");
-        frame.setBounds(800, 200, 320, 450); // Tamaño
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Para la finalizacion del programa
+            frame.setBounds(800, 200, 320, 450); // Tamaño
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Para la finalizacion del programa
 
 
         JTextField textResult = new JTextField("0");
-        textResult.setPreferredSize(new Dimension(250,60));
+            textResult.setPreferredSize(new Dimension(250,60));
         Font font = new Font("Arial", Font.PLAIN, 35);
-        textResult.setFont(font);
-        textResult.setHorizontalAlignment(SwingConstants.RIGHT);
+            textResult.setFont(font);
+            textResult.setHorizontalAlignment(SwingConstants.RIGHT);
 
 
 
         JPanel panelPrincipal = new JPanel();
-        panelPrincipal.setBackground(Color.LIGHT_GRAY);
+            panelPrincipal.setBackground(Color.LIGHT_GRAY);
 
         JPanel panelResultado = new JPanel();
-        panelResultado.setBackground(Color.LIGHT_GRAY);
-        panelResultado.setPreferredSize(new Dimension(300, 70));
-        panelResultado.setBorder(BorderFactory.createTitledBorder("💥-RESULTADO-💥"));
+            panelResultado.setBackground(Color.LIGHT_GRAY);
+            panelResultado.setPreferredSize(new Dimension(300, 70));
+            panelResultado.setBorder(BorderFactory.createTitledBorder("💥-RESULTADO-💥"));
 
 
         JPanel panelEspecia = new JPanel(new GridLayout(1, 2,10,10));
-        panelEspecia.setPreferredSize(new Dimension(250,80));
-        panelEspecia.setBackground(Color.LIGHT_GRAY);
+            panelEspecia.setPreferredSize(new Dimension(250,80));
+            panelEspecia.setBackground(Color.LIGHT_GRAY);
 
         JPanel panelBotones = new JPanel(new GridLayout(4, 4,10,10));
-        panelBotones.setPreferredSize(new Dimension(250,200));
-        panelBotones.setBackground(Color.LIGHT_GRAY);
+            panelBotones.setPreferredSize(new Dimension(250,200));
+            panelBotones.setBackground(Color.LIGHT_GRAY);
+
+
+
+/********************************************************************************************************************************************************************************************************************/
+
+        // OPCIONES UNICAMENTE DE BOTONES ESPECIALES PARA SALIR Y MINIMIZAR
+
+        Border border = BorderFactory.createBevelBorder(BevelBorder.RAISED, Color.lightGray, Color.darkGray);
 
 
 
 
+        // panel especial de las opciones
+        JPanel panelOpciones = new JPanel(new GridLayout(1,1,40,10));
+            panelPrincipal.add(panelOpciones);
 
+
+        // botonExit
+        JButton botonExit = new JButton("C" );
+            botonExit.setBackground(Color.decode("#DB9D00"));
+            botonExit.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED, Color.DARK_GRAY, Color.BLACK));
+            botonExit.setPreferredSize(new Dimension(40,40));
+            botonExit.setFocusPainted(false); // <-- para que no salga cuadradito rectangular al clicar
+            botonExit.setBorder(border);
+
+        // agregacion de boton exit al panel de opciones
+        panelOpciones.add(botonExit, BorderLayout.EAST);
+
+
+        // accion de estos botones
+
+        botonExit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+
+
+/********************************************************************************************************************************************************************************************************************/
 
 
 
@@ -61,62 +98,79 @@ public class Calculator {
 
 
         JButton botonC = new JButton("C");
-        botonC.setBackground(Color.decode("#DB9D00"));
-        botonC.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED, Color.DARK_GRAY, Color.BLACK));
+            botonC.setBackground(Color.decode("#DB9D00"));
+            botonC.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED, Color.DARK_GRAY, Color.BLACK));
+            botonC.setFocusPainted(false); // <-- para que no salga cuadradito rectangular al clicar
 
         JButton botonMasMenos = new JButton("+/-");
-        botonMasMenos.setBackground(Color.lightGray);
+            botonMasMenos.setBackground(Color.lightGray);
+            botonMasMenos.setFocusPainted(false); // <-- para que no salga cuadradito rectangular al clicar
 
 
         JButton botonDividir = new JButton("➗");
-        botonDividir.setBackground(Color.lightGray);
+            botonDividir.setBackground(Color.lightGray);
+            botonDividir.setFocusPainted(false); // <-- para que no salga cuadradito rectangular al clicar
 
         JButton botonMultiplicar = new JButton("❌");
-        botonMultiplicar.setBackground(Color.lightGray);
+            botonMultiplicar.setBackground(Color.lightGray);
+            botonMultiplicar.setFocusPainted(false); // <-- para que no salga cuadradito rectangular al clicar
 
         JButton boton7 = new JButton("7");
-        boton7.setBackground(Color.WHITE);
+            boton7.setBackground(Color.WHITE);
+            boton7.setFocusPainted(false); // <-- para que no salga cuadradito rectangular al clicar
 
         JButton boton8 = new JButton("8");
-        boton8.setBackground(Color.WHITE);
+            boton8.setBackground(Color.WHITE);
+            boton8.setFocusPainted(false); // <-- para que no salga cuadradito rectangular al clicar
 
         JButton boton9 = new JButton("9");
-        boton9.setBackground(Color.WHITE);
+            boton9.setBackground(Color.WHITE);
+            boton9.setFocusPainted(false); // <-- para que no salga cuadradito rectangular al clicar
 
         JButton botonRestar = new JButton("➖");
-        botonRestar.setBackground(Color.lightGray);
+            botonRestar.setBackground(Color.lightGray);
+            botonRestar.setFocusPainted(false); // <-- para que no salga cuadradito rectangular al clicar
 
         JButton boton4 = new JButton("4");
-        boton4.setBackground(Color.WHITE);
+            boton4.setBackground(Color.WHITE);
+            boton4.setFocusPainted(false); // <-- para que no salga cuadradito rectangular al clicar
 
         JButton boton5 = new JButton("5");
-        boton5.setBackground(Color.WHITE);
+            boton5.setBackground(Color.WHITE);
+            boton5.setFocusPainted(false); // <-- para que no salga cuadradito rectangular al clicar
 
         JButton boton6 = new JButton("6");
-        boton6.setBackground(Color.WHITE);
+            boton6.setBackground(Color.WHITE);
+            boton6.setFocusPainted(false); // <-- para que no salga cuadradito rectangular al clicar
 
         JButton botonSumar = new JButton("➕");
-        botonSumar.setBackground(Color.lightGray);
+            botonSumar.setBackground(Color.lightGray);
+            botonSumar.setFocusPainted(false); // <-- para que no salga cuadradito rectangular al clicar
 
         JButton boton1 = new JButton("1");
-        boton1.setBackground(Color.WHITE);
+            boton1.setBackground(Color.WHITE);
+            boton1.setFocusPainted(false); // <-- para que no salga cuadradito rectangular al clicar
 
         JButton boton2 = new JButton("2");
-        boton2.setBackground(Color.WHITE);
+            boton2.setBackground(Color.WHITE);
+            boton2.setFocusPainted(false); // <-- para que no salga cuadradito rectangular al clicar
 
         JButton boton3 = new JButton("3");
-        boton3.setBackground(Color.WHITE);
+            boton3.setBackground(Color.WHITE);
+            boton3.setFocusPainted(false); // <-- para que no salga cuadradito rectangular al clicar
 
         JButton boton0 = new JButton("0");
-        boton0.setBackground(Color.WHITE);
+            boton0.setFocusPainted(false); // <-- para que no salga cuadradito rectangular al clicar
+            boton0.setBackground(Color.WHITE);
 
         /***********************************************************************/
+
         JButton botonIgual = new JButton("=");
-        botonIgual.setBackground(Color.decode("#DB9D00"));
+            botonIgual.setBackground(Color.decode("#DB9D00"));
 
         JButton botonReset = new JButton("RESET");
-        botonReset.setBackground(Color.decode("#990014"));
-        botonReset.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED, Color.DARK_GRAY, Color.BLACK));
+            botonReset.setBackground(Color.decode("#990014"));
+            botonReset.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED, Color.DARK_GRAY, Color.BLACK));
 
 
 
@@ -164,8 +218,10 @@ public class Calculator {
 
 
 
+        frame.setUndecorated(true);
         frame.setVisible(true);
         frame.setResizable(false);
+
 
 
 
@@ -177,7 +233,11 @@ public class Calculator {
         boton0.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                textResult.setText("0");
+                if (textResult.getText().equals("0")){
+                    textResult.setText("0");
+                } else {
+                    textResult.setText(textResult.getText() + "0");
+                }
             }
         });
 
@@ -185,7 +245,11 @@ public class Calculator {
         boton1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                textResult.setText("1");
+                if (textResult.getText().equals("0")){
+                    textResult.setText("1");
+                } else {
+                    textResult.setText(textResult.getText() + "1");
+                }
             }
         });
 
@@ -193,7 +257,11 @@ public class Calculator {
         boton2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                textResult.setText("2");
+                if (textResult.getText().equals("0")){
+                    textResult.setText("2");
+                } else {
+                    textResult.setText(textResult.getText() + "2");
+                }
             }
         });
 
@@ -201,7 +269,11 @@ public class Calculator {
         boton3.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                textResult.setText("3");
+                if (textResult.getText().equals("0")){
+                    textResult.setText("3");
+                } else {
+                    textResult.setText(textResult.getText() + "3");
+                }
             }
         });
 
@@ -209,7 +281,11 @@ public class Calculator {
         boton4.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                textResult.setText("4");
+                if (textResult.getText().equals("0")){
+                    textResult.setText("4");
+                } else {
+                    textResult.setText(textResult.getText() + "4");
+                }
             }
         });
 
@@ -217,7 +293,11 @@ public class Calculator {
         boton5.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                textResult.setText("5");
+                if (textResult.getText().equals("0")){
+                    textResult.setText("5");
+                } else {
+                    textResult.setText(textResult.getText() + "5");
+                }
             }
         });
 
@@ -225,7 +305,11 @@ public class Calculator {
         boton6.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                textResult.setText("6");
+                if (textResult.getText().equals("0")){
+                    textResult.setText("6");
+                } else {
+                    textResult.setText(textResult.getText() + "6");
+                }
             }
         });
 
@@ -233,7 +317,11 @@ public class Calculator {
         boton7.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                textResult.setText("7");
+                if (textResult.getText().equals("0")){
+                    textResult.setText("7");
+                } else {
+                    textResult.setText(textResult.getText() + "7");
+                }
             }
         });
 
@@ -241,7 +329,11 @@ public class Calculator {
         boton8.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                textResult.setText("8");
+                if (textResult.getText().equals("0")){
+                    textResult.setText("8");
+                } else {
+                    textResult.setText(textResult.getText() + "8");
+                }
             }
         });
 
@@ -249,7 +341,11 @@ public class Calculator {
         boton9.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                textResult.setText("9");
+                if (textResult.getText().equals("0")){
+                    textResult.setText("9");
+                } else {
+                    textResult.setText(textResult.getText() + "9");
+                }
             }
         });
 
@@ -320,27 +416,27 @@ public class Calculator {
                 double numero2 = Double.parseDouble(textResult.getText());
 
                 switch (operador){
-                    case "+":
+                    case "+": // <-- situacion SUMA
                         double resultado = numero1 + numero2;
                         textResult.setText(Double.toString(resultado));
                         break;
 
-                    case "-":
+                    case "-": // <-- situacion RESTA
                         resultado = numero1 - numero2;
                         textResult.setText(Double.toString(resultado));
                         break;
 
-                    case "*":
+                    case "*": // <-- situacion MULTIPLICACION
                         resultado = numero1 * numero2;
                         textResult.setText(Double.toString(resultado));
                         break;
 
-                    case "/":
+                    case "/": // <-- situacion DIVISION
                         resultado = numero1 / numero2;
                         textResult.setText(Double.toString(resultado));
                         break;
 
-                    default: textResult.setText("El orden de las operaciones no es correcto");
+                    default: textResult.setText("ERROR");
                 }
 
 
