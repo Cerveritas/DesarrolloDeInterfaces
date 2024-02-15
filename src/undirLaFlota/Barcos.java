9d99784fdc48be349abede272764d89a90445124
SergioCerveraJ <seceji@gmail.com>
2024-02-15 10:22:07 +0100

    15/02/24
    Undir la flota add

---
+++ src/undirLaFlota/Barcos.java
@@ -0,0 +1,69 @@
+package undirLaFlota;
+
+public class Barcos {
+
+    private String Submarino;
+    private String Destructor;
+    private String Acorazado;
+    private String Portaviones;
+    private String fragata;
+    private String patera;
+
+    public Barcos(String submarino, String destructor, String acorazado, String portaviones, String fragata, String patera) {
+        Submarino = submarino;
+        Destructor = destructor;
+        Acorazado = acorazado;
+        Portaviones = portaviones;
+        this.fragata = fragata;
+        this.patera = patera;
+    }
+
+
+    public String getSubmarino() {
+        return Submarino;
+    }
+
+    public void setSubmarino(String submarino) {
+        Submarino = submarino;
+    }
+
+    public String getDestructor() {
+        return Destructor;
+    }
+
+    public void setDestructor(String destructor) {
+        Destructor = destructor;
+    }
+
+    public String getAcorazado() {
+        return Acorazado;
+    }
+
+    public void setAcorazado(String acorazado) {
+        Acorazado = acorazado;
+    }
+
+    public String getPortaviones() {
+        return Portaviones;
+    }
+
+    public void setPortaviones(String portaviones) {
+        Portaviones = portaviones;
+    }
+
+    public String getFragata() {
+        return fragata;
+    }
+
+    public void setFragata(String fragata) {
+        this.fragata = fragata;
+    }
+
+    public String getPatera() {
+        return patera;
+    }
+
+    public void setPatera(String patera) {
+        this.patera = patera;
+    }
+}
---
+++ src/undirLaFlota/GUImain.java
@@ -0,0 +1,33 @@
+package undirLaFlota;
+
+import javax.swing.*;
+
+public class GUImain {
+    private JPanel PanelPrincipal;
+    private JPanel PanelMio;
+    private JPanel PanelEnemigo;
+    private JPanel panelLinea1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JButton button10;
+    private JPanel PanelOpciones;
+    private JButton button87Button;
+    private JButton kjl;
+    private JButton kljl;
+    private JButton kljlkjh;
+    private JButton jkghjkh;
+    private JButton ihyjhj;
+    private JButton yutyut;
+    private JButton nbnbç;
+    private JButton hjghjg;
+    private JButton iiii;
+    private JPanel panelLinea2;
+    private JPanel panelLinea3;
+}
